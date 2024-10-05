2ffc02d0ab47a0055e25968bce7de84f3ddbf919
Noman Ali <97123202+nomaanali06@users.noreply.github.com>
2024-10-05 00:43:06 -0700

    Corrected indentation

--- Gulberg.py
+++ Gulberg.py
@@ -1,200 +1,111 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 import selenium
 from selenium import webdriver as wb
-import time
 import math
-
-
-# In[3]:
-
-
-links=[]
-url='https://www.zameen.com/Plots/Islamabad_Gulberg-1682-1.html'
-webD = wb.Chrome('C:/Users/ali/Downloads/Compressed/chromedriver')
-webD.get(url)
-#webD.refresh()
-product_info_list=webD.find_elements_by_class_name('ef447dde')
-pages = webD.find_element_by_class_name("_2aa3d08d").text
-d1=[]
-tplots=""
-i=0
-while(pages[i]!=' '):
-    if pages[i].isdigit():
-        d1.append((pages[i]))
-        tplots=tplots+pages[i]
-    i=i+1
-    
-tplots=int(tplots)    
-temp=0
-for i in product_info_list:
-    p1=product_info_list[temp]
-    p2=p1.find_element_by_tag_name('a')   #for reference h ref
-    links.append(p2.get_property('href'))
-    temp=temp+1
-    if(len(links)>=tplots):
-        break
-    
-
-
-print(pages) 
-
-
-# In[ ]:
-
-
-
-
-
-# In[4]:
-
-
-d1=[]
-tplots=""
-i=0
-while(pages[i]!=' '):
-    if pages[i].isdigit():
-        d1.append((pages[i]))
-        tplots=tplots+pages[i]
-    i=i+1
-
-tplots=int(tplots)
-npages=math.ceil(tplots/25)
-print(npages)
-
-
-# In[ ]:
-
-
-if(npages>1):
-    for i in range(2,npages+1):
-        url='https://www.zameen.com/Plots/Islamabad_Gulberg-1682-'+ str(i)+'.html'
-        webD = wb.Chrome('C:/Users/ali/Downloads/Compressed/chromedriver')
-        webD.get(url)
-        #webD.refresh()
-        product_info_list=webD.find_elements_by_class_name('ef447dde')
-        temp=0
-        for i in product_info_list:
-            p1=product_info_list[temp]
-            p2=p1.find_element_by_tag_name('a')   #for reference h ref
-            links.append(p2.get_property('href'))
-            temp=temp+1 
-            if(len(links)>=tplots):
-               break
-        
-    
-
-
-# In[ ]:
-
-
-len(links)
-
-
-# In[ ]:
-
-
-from tqdm import tqdm
-details=[]
-
-for i in tqdm(links):
-    webD.get(i)
-    
-    Time=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[8]/span[2]').text
-    Bath=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[7]/span[2]').text
-    a_string=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[1]/div/div/span').text
-
-    id1 = []
-    for word in a_string.split():
-         if word.isdigit():
-            id1.append(int(word))
-    price=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[2]/div[1]/div/form/div[1]/div/div[1]/div/div/span[3]').text
-    type1=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[1]/span[2]').text
-    area=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[5]/span[2]').text
-    purpose=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[6]/span[2]').text
-    Location=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[3]/span[2]').text
-    Bedroom=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[7]/span[2]').text                                       
-    Added=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[2]/div[2]/div[1]').text
-    #time.sleep(5)
-    #months1=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[8]/div/div[2]/div/div[1]/div[1]/div/div[1]/ul/li[2]/ul/li[1]/span[2]').text
-    #months2=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[8]/div/div[2]/div/div[1]/div[1]/div/div[1]/ul/li[2]/ul/li[2]/span[2]').text
-    #months3=webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[8]/div/div[2]/div/div[1]/div[1]/div/div[1]/ul/li[2]/ul/li[3]/span[2]').text
-    
-    
-    list1={"id":id1[0],
-    "Type":type1,
-      "Area":area,
-      "Price":price,
-      "Purpose":purpose,
-      "Location":Location,
-      "Bedroom":Bedroom,
-      "Bath":Bath,
-      "Added":Added,
-           "time":Time,
-     # "6months":months1,
-     # "12months":months2,
-      # "24months":months3,
-       "link":i
-      
-      }
-    details.append(list1)
-    
-    
-
-
-# In[ ]:
-
-
-webD.close()
-
-
-# In[ ]:
-
-
-import pandas as pd
-dataset=pd.DataFrame(details)
-
-
-# In[ ]:
-
-
-def lactomillion(abc):
-        million = re.findall(r"[-+]?\d*\.\d+|\d+",abc )
-        million1=float(million[0])
-        million1=million1/10
-        dataset['Price'][i]=million1 
-        
-def croretomillion(abc):
-        million = re.findall(r"[-+]?\d*\.\d+|\d+",abc )
-        million1=float(million[0])
-        million1=million1*10
-        dataset['Price'][i]=million1 
-                
-
-
-# In[ ]:
-
-
 import re
+import pandas as pd
+from tqdm import tqdm
 
-for i in range(0,len(dataset['Price'])):
-    word = str(dataset['Price'][i])
-    abc=str(dataset['Price'][i])
-    if (word.find('Lakh') != -1): 
-            lactomillion(abc)
+# Function to convert 'Lakh' and 'Crore' values to 'Million'
+def convert_price_to_million(price_str):
+    """Convert price in 'Lakh' or 'Crore' to 'Million'."""
+    number_str = re.findall(r"[-+]?\d*\.\d+|\d+", price_str)  # Extract numeric values from string
+    if number_str:  # If any numbers found
+        amount = float(number_str[0])
+        if 'Lakh' in price_str:
+            return amount / 10  # Convert Lakh to Million
+        elif 'Crore' in price_str:
+            return amount * 10  # Convert Crore to Million
+    return 0.0  # Default if no match found
 
-            
-    elif (word.find('Crore') != -1): {
-            croretomillion(abc)
-     }
+# Function to scrape plot links from the initial page
+def scrape_initial_page(url):
+    """Scrape links to plots from the initial page."""
+    links = []  # List to hold plot links
+    webD = wb.Chrome('C:/Users/ali/Downloads/Compressed/chromedriver')
+    webD.get(url)
 
+    # Extract total number of plots
+    pages = webD.find_element_by_class_name("_2aa3d08d").text
+    total_plots = int(''.join(filter(str.isdigit, pages)))  # Extract digits to get total plots
 
-# In[ ]:
+    # Extract links from the first page
+    product_info_list = webD.find_elements_by_class_name('ef447dde')
+    for item in product_info_list:
+        link = item.find_element_by_tag_name('a').get_property('href')  # Get the plot link
+        links.append(link)  # Add to the links list
+        if len(links) >= total_plots:  # Stop if we have enough links
+            break
+    
+    # Calculate total pages needed
+    npages = math.ceil(total_plots / 25)  # 25 plots per page
+    webD.quit()  # Close the driver
+    
+    return links, npages  # Return links and number of pages
 
+# Function to scrape details from each plot link
+def scrape_plot_details(links):
+    """Scrape plot details from the given links."""
+    details = []  # List to hold plot details
+    webD = wb.Chrome('C:/Users/ali/Downloads/Compressed/chromedriver')
 
+    for link in tqdm(links):  # Progress bar for links scraping
+        webD.get(link)  # Open the plot link
 
+        # Extract plot details using XPath
+        time_added = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[8]/span[2]').text
+        bath = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[7]/span[2]').text
+        id_str = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[1]/div/div/span').text
+        price = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[2]/div[1]/div/form/div[1]/div/div[1]/div/div/span[3]').text
+        type1 = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[1]/span[2]').text
+        area = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[5]/span[2]').text
+        purpose = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[6]/span[2]').text
+        location = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[3]/span[2]').text
+        bedroom = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[1]/div[4]/div/div[2]/ul/li[7]/span[2]').text
+        added = webD.find_element_by_xpath('//*[@id="body-wrapper"]/main/div[4]/div[2]/div[2]/div[1]').text
 
+        # Extract numeric ID from the string
+        id_number = [int(word) for word in id_str.split() if word.isdigit()]
+
+        # Create a dictionary to hold plot details
+        details.append({
+            "id": id_number[0] if id_number else None,
+            "Type": type1,
+            "Area": area,
+            "Price": convert_price_to_million(price),  # Convert price to million
+            "Purpose": purpose,
+            "Location": location,
+            "Bedroom": bedroom,
+            "Bath": bath,
+            "Added": added,
+            "time": time_added,
+            "link": link
+        })
+
+    webD.quit()  # Close the driver
+    return details  # Return scraped details
+
+# Main execution starts here
+initial_url = 'https://www.zameen.com/Plots/Islamabad_G__8-170-1.html'
+links, total_pages = scrape_initial_page(initial_url)
+
+# If there are more pages, scrape links from additional pages
+if total_pages > 1:
+    for i in range(2, total_pages + 1):
+        url = f'https://www.zameen.com/Plots/Islamabad_Gulberg-1682-{i}.html'
+        additional_links, _ = scrape_initial_page(url)  # Scrape additional links
+        links.extend(additional_links)  # Add to the main list of links
+
+# Scrape detailed information from each plot link
+details = scrape_plot_details(links)
+
+# Convert details to a DataFrame
+dataset = pd.DataFrame(details)
+
+# Save the dataset to a CSV file
+with open('zameen.csv', 'a', newline='') as f:
+    dataset.to_csv(f, header=f.tell() == 0, index=False)  # Avoid writing header on append
+
+print("Scraping completed. Data saved to 'zameen.csv'.")
